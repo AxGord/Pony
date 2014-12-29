97813105c3ce53833968749a16a8305dac3ab0b9
AxG <axgord@gmail.com>
2014-12-29 17:50:18 +0500

    bar

---
+++ pony/flash/ui/Bar.hx
@@ -0,0 +1,44 @@
+package pony.flash.ui;
+
+import flash.display.MovieClip;
+import flash.events.MouseEvent;
+import pony.events.Signal;
+import pony.events.Signal1;
+
+/**
+ * Bar
+ * @author AxGord
+ */
+class Bar extends MovieClip implements FLSt {
+
+	@:st private var bar:MovieClip;
+	
+	private var total:Float;
+	
+	public var value(default, set):Float = 0;
+	public var on:Signal1<Bar, Float>;
+	
+	public function new() {
+		super();
+		on = Signal.create(this);
+		FLTools.init < init;
+		addEventListener(MouseEvent.CLICK, clickHandler);
+	}
+	
+	private function clickHandler(_):Void {
+		value = mouseX / total;
+	}
+	
+	private function init():Void {
+		total = width;
+		bar.width = 0;
+	}
+	
+	private function set_value(v:Float):Float {
+		if (value == v) return v;
+		on.dispatch(v);
+		bar.width = v * total;
+		return value = v;
+	}
+	
+}
--- pony/flash/ui/SongPlayer.hx
+++ pony/flash/ui/SongPlayer.hx
@@ -1,8 +1,12 @@
 package pony.flash.ui;
 import flash.display.MovieClip;
+import flash.events.Event;
+import flash.events.ProgressEvent;
 import flash.media.Sound;
+import flash.media.SoundChannel;
 import flash.net.URLRequest;
 import flash.text.TextField;
+import pony.time.DeltaTime;
 import pony.time.Time;
 
 typedef SongInfo = {
@@ -18,7 +22,9 @@ typedef SongInfo = {
  */
 class SongPlayer extends MovieClip implements FLSt {
 
-	@:st private var progress:ProgressBar;
+	//@:st private var progress:ProgressBar;
+	@:st private var playBar:Bar;
+	@:st private var loadProgress:ProgressBar;
 	@:st private var bPlay:Button;
 	@:st private var tTitle:TextField;
 	@:st private var bMute:Button;
@@ -26,14 +32,21 @@ class SongPlayer extends MovieClip implements FLSt {
 	
 	public var isPlay(get, set):Bool;
 	
+	private var sound:Sound;
+	private var channel:SoundChannel;
+	
 	public function new() {
 		super();
 		visible = false;
 		FLTools.init < init;
+		sound = new Sound();
+		sound.addEventListener(ProgressEvent.PROGRESS, progressHandler);
+		sound.addEventListener(Event.SOUND_COMPLETE, soundComplete);
 	}
 	
 	private function init() {
-		volume.total = 500;
+		volume.total = 1000;
+		
 		volume.update.add(volumeHandler);
 		if (bMute != null) bMute.core.click.add(volume.set_position);
 		bPlay.core.sw = [2, 1, 0];
@@ -46,8 +59,8 @@ class SongPlayer extends MovieClip implements FLSt {
 	}
 	
 	private function volumeHandler(v:Float):Void {
-		v /= 500;
-		
+		v /= 1000;
+		trace(v);
 	}
 	
 	public static function formatSong(song:SongInfo):String return (song.author != null ? song.author + ' - ' : '') + song.title;
@@ -55,9 +68,16 @@ class SongPlayer extends MovieClip implements FLSt {
 	public function loadSong(song:SongInfo):Void {
 		visible = true;
 		tTitle.text = SongPlayer.formatSong(song);
-		var req:URLRequest = new URLRequest(song.file);
-		var sound:Sound = new Sound(req);
+		sound.load(new URLRequest(song.file));
 		if (isPlay) playSong();
+	}
+	
+	private function progressHandler(event:ProgressEvent):Void {
+		loadProgress.progress = event.bytesLoaded / event.bytesTotal;
+	}
+	
+	private function soundComplete(_):Void {
+		trace('soundComplete');
 	}
 	
 	private function get_isPlay():Bool return bPlay.core.mode == 2;
@@ -74,10 +94,20 @@ class SongPlayer extends MovieClip implements FLSt {
 	
 	private function playSong():Void {
 		trace('play song');
+		channel = sound.play();
+		DeltaTime.fixedUpdate << update;
 	}
 	
 	private function pauseSong():Void {
 		trace('pause song');
+		channel.stop();
+		DeltaTime.fixedUpdate >> update;
+	}
+	
+	private function update():Void {
+		//progress.progress = channel.position / sound.length;
+		playBar.value = channel.position / sound.length;
+		trace(channel.position / sound.length);
 	}
 	
 }
