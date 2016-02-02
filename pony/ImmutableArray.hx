e1560c018b2645b35b1cb525523b4157ed956a8f
AxG <axgord@gmail.com>
2016-02-02 21:17:43 +0300

    ImmutableArray to Iterable

--- pony/ImmutableArray.hx
+++ pony/ImmutableArray.hx
@@ -33,6 +33,6 @@ package pony;
  * @author axgord@gmail.com
  */
 @:forward(length, concat, join, toString, indexOf, lastIndexOf, copy, iterator, map, filter)
-abstract ImmutableArray<T>(Array<T>) from Array<T> {
+abstract ImmutableArray<T>(Array<T>) from Array<T> to Iterable<T> {
 	@:arrayAccess @:extern inline public function arrayAccess(key:Int):T return this[key];
 }
