6a30f331c0b7df3cf4db4142d76f1c4529bccc5d
Alexander Gordeyko <axgord@gmail.com>
2017-04-30 00:48:15 +0300

    Pony Tools for Mac OS

--- install/src/Main.hx
+++ install/src/Main.hx
@@ -16,15 +16,27 @@ class Main {
 		toolsSrc = StringTools.replace(toolsSrc, '/', PD);
 		var toolshome = toolsSrc + PD + 'bin' + PD;
 		
+		Sys.println('Install haxelibs');		
+		for (m in ['hxnodejs'])
+			Sys.command('haxelib', ['install', m]);
+
 		Sys.println('Compile pony.exe');
 		
 		Sys.command('haxe', ['--cwd', toolsSrc, 'build.hxml']);
 		
 		FileSystem.deleteFile(toolshome + 'pony.n');
-		
+
+		//Sys.command('sudo', ['chmod', '/usr/local/lib/node_modules', '777']);
+
+		var npm:Array<String> = ['poeditor-client', 'uglify-js'];
+
+		Sys.println('Install npm');
+
 		switch Sys.systemName() {
 			case 'Windows':
-		
+
+				for (m in npm) Sys.command('npm', ['-g', 'install', m]);		
+
 				var path = Sys.getEnv('PATH');
 				
 				Sys.println('Add user path to pony.exe');
@@ -44,6 +56,32 @@ class Main {
 				Sys.command('install'+PD+'append_user_path.cmd', [toolshome]);
 				
 				Sys.println('Installation complete, please reenter in command line and use pony');
+
+			case 'Mac':
+
+				for (m in npm) Sys.command('sudo', ['npm', '-g', 'install', m]);
+
+				Sys.println('Add user path to ponytools');
+				var home = Sys.getEnv('HOME');
+				var pFile = home + '/.bash_profile';
+				var npmPath = '/usr/local/lib/node_modules';
+				
+				var data = [
+					"export NODE_PATH="+npmPath,
+					"export PONYTOOLS_PATH="+toolshome,
+					"export PATH=$PATH:$PONYTOOLS_PATH"
+				];
+
+				if (FileSystem.exists(pFile)) {
+					var c = File.getContent(pFile);
+					if (c.indexOf('PONYTOOLS_PATH') == -1) {
+						File.saveContent(pFile, c + "\n" + data.join('\n'));
+					}
+				} else {
+					File.saveContent(pFile, data.join('\n'));
+				}
+
+				Sys.println('Installation complete, please reenter in command line and use pony');				
 
 			case _:
 				Sys.println('Not supported OS');
