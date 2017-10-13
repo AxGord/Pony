f2bd352ca685a7699066f12732c16142c5012c6e
Alexander Gordeyko <axgord@gmail.com>
2017-10-13 18:45:05 +0300

    Tools: pixi create

--- tools/src/Create.hx
+++ tools/src/Create.hx
@@ -8,13 +8,14 @@ enum ProjectType {
 class Create {
 
 	private static inline var outputDir:String = 'bin/';
+	private static inline var outputFile:String = 'app';
 
 	public static function run(args:Array<String>):Void {
 
 		var type:ProjectType = try ProjectType.createByName(args[0]) catch(_:Any) null;
 		var name:String = type != null ? args[1] : args[0];
 
-		if (FileSystem.exists(Utils.MAIN_FILE)) Utils.error(Utils.MAIN_FILE + ' exists');
+		//if (FileSystem.exists(Utils.MAIN_FILE)) Utils.error(Utils.MAIN_FILE + ' exists');
 				
 		var content = Xml.createDocument();
 		var root = Xml.createElement('project');
@@ -22,16 +23,40 @@ class Create {
 		switch type {
 			case pixi:
 				root.addChild(serverElement(true));
-				//root.addChild(build);
+				
+				var download = Xml.createElement('download');
+				download.set('path', 'jslib/');
+				var unit = Xml.createElement('unit');
+				unit.set('url', 'https://raw.githubusercontent.com/stacktracejs/stacktrace.js/v{v}/dist/stacktrace.min.js');
+				unit.set('v', '1.3.0');
+				download.addChild(unit);
+				var unit = Xml.createElement('unit');
+				unit.set('url', 'https://pixijs.download/v{v}/pixi.min.js');
+				unit.set('check', 'pixi.js - v{v}');
+				unit.set('v', '4.5.3');
+				download.addChild(unit);
+				root.addChild(download);
+
+				var haxelib = Xml.createElement('haxelib');
+				haxelib.addChild(Utils.xmlSimple('lib', 'pixijs 4.5.5'));
+				haxelib.addChild(Utils.xmlSimple('lib', 'pony 0.4.7'));
+				root.addChild(haxelib);
+
+				var build = Xml.createElement('build');
+				build.set('hxml', 'true');
+				build.addChild(Utils.xmlSimple('js', outputDir + outputFile + '.js'));
+				build.addChild(Utils.xmlSimple('cp', 'src'));
+				build.addChild(Utils.xmlSimple('main', 'Main'));
+				build.addChild(Utils.xmlSimple('dce', 'full'));
+				build.addChild(Utils.xmlSimple('d', 'analyzer-optimize'));
+				root.addChild(build);
 
 			case null:
 				root.addChild(Xml.createComment('Put configuration here'));
 		}
 		content.addChild(root);
-		var r = '<?xml version="1.0" encoding="utf-8" ?>\n';
-		r += haxe.xml.Printer.print(content, true);
-		File.saveContent(Utils.MAIN_FILE, r);
-
+		
+		Utils.savePonyProject(content);
 		//createVscode();
 
 	}
@@ -39,18 +64,11 @@ class Create {
 	private static function serverElement(httpServer:Bool):Xml {
 		var r = Xml.createElement('server');
 		if (httpServer) {
-			r.addChild(xmlSimple('path', outputDir));
-			r.addChild(xmlSimple('port', '2000'));
+			r.addChild(Utils.xmlSimple('path', outputDir));
+			r.addChild(Utils.xmlSimple('port', '2000'));
 		}
-		r.addChild(xmlSimple('haxe', '6001'));
+		r.addChild(Utils.xmlSimple('haxe', '6001'));
 		return r;
-	}
-
-	private static function xmlSimple(v:String, data:String):Xml {
-		var e = Xml.createElement(v);
-		var d = Xml.createPCData(data);
-		e.addChild(d);
-		return e;
 	}
 
 	private static function createVscode():Void {
--- tools/src/Utils.hx
+++ tools/src/Utils.hx
@@ -10,6 +10,9 @@ class Utils {
 	
 	public static inline var MAIN_FILE:String = 'pony.xml';
 	
+	public static inline var XML_REMSP_LEFT:String = '{REMSP_LEFT}';
+	public static inline var XML_REMSP_RIGHT:String = '{REMSP_RIGHT}';
+
 	public static function parseArgs(args:Array<String>):AppCfg {
 		var debug = args.indexOf('debug') != -1;
 		var app:String = null;
@@ -38,8 +41,36 @@ class Utils {
 				tdata = ndata;
 			}
 		}
-		sys.io.File.saveContent(file, tdata);
+		File.saveContent(file, tdata);
 		Sys.println('$file saved');
+	}
+
+	public static function saveXML(file:String, xml:Xml):Void {
+		var doc = Xml.createDocument();
+		doc.addChild(Xml.createProcessingInstruction('xml version="1.0" encoding="utf-8"'));
+		doc.addChild(xml);
+		var r = haxe.xml.Printer.print(doc, true);
+		
+		var a = r.split(XML_REMSP_LEFT);
+		r = '';
+		for (e in a) r += e.substring(0, e.lastIndexOf('>') + 1);
+		a = r.split(XML_REMSP_RIGHT);
+		r = '';
+		for (e in a) r += e.substring(e.indexOf('<'));
+
+		File.saveContent(file, r);
+	}
+
+	public static function savePonyProject(xml:Xml):Void saveXML(MAIN_FILE, xml);
+
+	public static function xmlData(data:String):Xml {
+		return Xml.createPCData(XML_REMSP_LEFT + data + XML_REMSP_RIGHT);
+	}
+
+	public static function xmlSimple(v:String, data:String):Xml {
+		var e = Xml.createElement(v);
+		e.addChild(Utils.xmlData(data));
+		return e;
 	}
 	
 }
