f1d8a0e383bb97f72ad0957507ec65d3084eece0
Alexander Gordeyko <axgord@gmail.com>
2017-10-31 21:56:01 +0300

    Update licenses

--- pony/net/http/modules/mmodels/actions/Insert.hx
+++ pony/net/http/modules/mmodels/actions/Insert.hx
@@ -1,16 +1,16 @@
 /**
-* Copyright (c) 2012-2016 Alexander Gordeyko <axgord@gmail.com>. All rights reserved.
-*
+* Copyright (c) 2012-2017 Alexander Gordeyko <axgord@gmail.com>. All rights reserved.
+* 
 * Redistribution and use in source and binary forms, with or without modification, are
 * permitted provided that the following conditions are met:
-*
-*   1. Redistributions of source code must retain the above copyright notice, this list of
-*      conditions and the following disclaimer.
-*
-*   2. Redistributions in binary form must reproduce the above copyright notice, this list
-*      of conditions and the following disclaimer in the documentation and/or other materials
-*      provided with the distribution.
-*
+* 
+* 1. Redistributions of source code must retain the above copyright notice, this list of
+*   conditions and the following disclaimer.
+* 
+* 2. Redistributions in binary form must reproduce the above copyright notice, this list
+*   of conditions and the following disclaimer in the documentation and/or other materials
+*   provided with the distribution.
+* 
 * THIS SOFTWARE IS PROVIDED BY ALEXANDER GORDEYKO ``AS IS'' AND ANY EXPRESS OR IMPLIED
 * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
 * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL ALEXANDER GORDEYKO OR
@@ -20,10 +20,6 @@
 * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*
-* The views and conclusions contained in the software and documentation are those of the
-* authors and should not be interpreted as representing official policies, either expressed
-* or implied, of Alexander Gordeyko <axgord@gmail.com>.
 **/
 package pony.net.http.modules.mmodels.actions;
 
