e53259908535dd17ce0d4eb898d8e608915e9f69
Alexander Gordeyko <axgord@gmail.com>
2017-12-27 14:52:02 +0300

    RPCUnit: remove debug info

--- pony/net/rpc/RPCUnit.hx
+++ pony/net/rpc/RPCUnit.hx
@@ -3,22 +3,18 @@ package pony.net.rpc;
 import haxe.io.BytesOutput;
 import haxe.io.BytesInput;
 import haxe.io.Bytes;
+import pony.magic.HasSignal;
+import pony.events.Signal1;
 
 /**
  * RPCUnit
  * @author AxGord <axgord@gmail.com>
  */
-class RPCUnit<T:pony.net.rpc.IRPC> extends RPCBase<T> {
+class RPCUnit<T:pony.net.rpc.IRPC> extends RPCBase<T> implements HasSignal {
 
+	@:auto public var onData:Signal1<Bytes>;
 	private function new() super();
-	private function send():Void {
-		var b = pack();
-		trace(b.length);
-		onData(b);
-	}
-	public dynamic function onData(data:Bytes):Void {}
-	public function data(b:Bytes):Void {
-		trace(b.length);
-		dataHandler(new BytesInput(b));
-	}
+	private function send():Void eData.dispatch(pack());
+	public function data(b:Bytes):Void dataHandler(new BytesInput(b));
+	
 }
