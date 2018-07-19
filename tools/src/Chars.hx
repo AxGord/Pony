814d777aef8ad8d1793fa9499164c49da806650a
Alexander Gordeyko <axgord@gmail.com>
2018-07-19 13:57:25 +0300

    haxe 3 fix

--- tools/src/Chars.hx
+++ tools/src/Chars.hx
@@ -30,8 +30,13 @@ class Chars {
 	public static function run(file:String):Void {
 		var c:String = sys.io.File.getContent(file);
 		var x:Fast = new Fast(Xml.parse(c));
+		#if (haxe_ver >= "4.0.0")
 		var chars:Array<Fast> = x.node.font.node.chars.nodes.char;
 		var rs:Utf8 = new Utf8(chars.length);
+		#else
+		var chars:List<Fast> = x.node.font.node.chars.nodes.char;
+		var rs:Utf8 = new Utf8();
+		#end
 		for (char in chars)
 			rs.addChar(Std.parseInt(char.att.id));
 		Sys.println(rs.toString());
