d993183c6a3a4a1ea795aaee9712684562e59602
Alexander Gordeyko <axgord@gmail.com>
2018-09-10 20:21:28 +0300

    gradient alpha

--- pony/pixi/ui/Gradient.hx
+++ pony/pixi/ui/Gradient.hx
@@ -34,6 +34,7 @@ import pony.pixi.App;
 import pony.geom.IWH;
 import pony.geom.Point;
 import pony.color.UColors;
+import pony.color.UColor;
 import pony.magic.HasLink;
 
 class Gradient extends Sprite implements IWH implements HasLink {
@@ -52,14 +53,22 @@ class Gradient extends Sprite implements IWH implements HasLink {
 			canvas.width = 1;
 			canvas.height = l;
 			for (i in 0...l) {
-				ctx.fillStyle = colors[i];
+				var c:UColor = colors[i];
+				if (c.a != 0)
+					ctx.fillStyle = 'rgba(${c.r}, ${c.g}, ${c.b}, ${c.invertAlpha.af})';
+				else
+					ctx.fillStyle = colors[i];
 				ctx.fillRect(0, i, 1, 1);
 			}
 		} else {
 			canvas.width = l;
 			canvas.height = 1;
 			for (i in 0...l) {
-				ctx.fillStyle = colors[i];
+				var c:UColor = colors[i];
+				if (c.a != 0)
+					ctx.fillStyle = 'rgba(${c.r}, ${c.g}, ${c.b}, ${c.invertAlpha.af})';
+				else
+					ctx.fillStyle = colors[i];
 				ctx.fillRect(i, 0, 1, 1);
 			}
 		}
