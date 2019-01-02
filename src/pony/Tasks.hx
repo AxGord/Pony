bbae1adb5dbe5dc62151f9d35e5846c6b561c816
Alexander Gordeyko <axgord@gmail.com>
2019-01-02 19:10:22 +0300

    fix

--- src/pony/Tasks.hx
+++ src/pony/Tasks.hx
@@ -8,8 +8,8 @@ abstract Tasks(Pair<Int, Void -> Void>) {
 	
 	public var ready(get, never):Bool;
 	public inline function new(cb:Void -> Void) this = new Pair(0, cb);
-	public function add():Void this.a++;
-	public function end():Void if (--this.a == 0) this.b();
+	public function add():Void this.a += 1;
+	public function end():Void if ((this.a -= 1) == 0) this.b();
 	@:extern private inline function get_ready():Bool return this.a == 0;
 	
 }
