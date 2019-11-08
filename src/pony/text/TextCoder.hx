094a4c42faafe71b40a777170ff04a6a6ba17054
Alexander Gordeyko <axgord@gmail.com>
2019-11-08 17:44:02 +0300

    Upgrade text coder

--- src/pony/text/TextCoder.hx
+++ src/pony/text/TextCoder.hx
@@ -5,42 +5,51 @@ package pony.text;
  * @author AxGord
  */
 class TextCoder {
-	
-	static private var chars:String = '0123456789QWERTYUIOPASDFGHJKLZXCVBNM';
-	
-	private var key:String;
-	
-	public function new(key:String) {
-		this.key = key.toUpperCase();
+
+	private static var numbers: String = '0123456789';
+	private static var letters: String = 'QWERTYUIOPASDFGHJKLZXCVBNM';
+	private static var symbols: String = '~!@#$%^&*()_+|{:"<>?`-=\\[];\',./№';
+
+	private var key: String;
+	private var allowLowercase: Bool;
+	private var chars: String;
+
+	public function new(key: String, ?allowLowercase: Bool, ?allowSymbols: Bool) {
+		this.key = allowLowercase ? key : key.toUpperCase();
+		this.allowLowercase = allowLowercase;
+		chars = numbers + letters;
+		if (allowLowercase) chars += letters.toLowerCase();
+		if (allowSymbols) chars += symbols;
 	}
-	
-	public function encode(text:String):String {
-		var s:String = core(text);
-		if (text.toUpperCase() == core(s, -1))
+
+	public function encode(text: String): String {
+		if (!allowLowercase) text = text.toUpperCase();
+		var s: String = core(text);
+		if (text == core(s, -1))
 			return s;
 		else
 			return null;
 	}
-	
-	public function decode(text:String, k=null):String {
-		var s:String = core(text, -1);
-		if (text.toUpperCase() == core(s))
+
+	public function decode(text: String, k = null): String {
+		if (!allowLowercase) text = text.toUpperCase();
+		var s: String = core(text, -1);
+		if (text == core(s))
 			return s;
 		else
 			return null;
 	}
-	
-	private function core(text:String, mode:Int = 1):String {
+
+	private function core(text: String, mode: Int = 1): String {
 		if (text == '') return null;
-		var n:Int = 0;
-		text = text.toUpperCase();
-		var s:String = '';
+		var n: Int = 0;
+		var s: String = '';
 		for (i in 0...text.length) {
 			if (n >= key.length)
 				n = 0;
-			var tp:Int = chars.indexOf(text.charAt(i));
-			var kp:Int = chars.indexOf(key.charAt(n));
-			var np:Int = tp + kp * mode;
+			var tp: Int = chars.indexOf(text.charAt(i));
+			var kp: Int = chars.indexOf(key.charAt(n));
+			var np: Int = tp + kp * mode;
 			if (np >= chars.length)
 				np -= chars.length;
 			if (np < 0)
@@ -50,5 +59,5 @@ class TextCoder {
 		}
 		return s;
 	}
-	
+
 }
