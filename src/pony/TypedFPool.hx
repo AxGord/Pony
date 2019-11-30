09d2b698e4952c72e9943f1db64a8644b83a85db
Alexander Gordeyko <axgord@gmail.com>
2019-11-30 04:13:34 +0300

    Typed object pool, using create function

---
+++ src/pony/TypedFPool.hx
@@ -0,0 +1,26 @@
+package pony;
+
+/**
+ * Typed object pool, using create function
+ * @author AxGord <axgord@gmail.com>
+ */
+@:nullSafety(Strict) class TypedFPool<T> implements IPool<T> {
+
+	private var list:Array<T> = [];
+	private var fn: Void -> T;
+
+	public function new(fn: Void -> T) this.fn = fn;
+
+	public inline function get(): T {
+		var v: Null<T> = list.pop();
+		return v == null ? fn() : v;
+	}
+
+	public inline function ret(obj: T):Void list.push(obj);
+
+	@:nullSafety(Off) public function destroy():Void {
+		list = null;
+		fn = null;
+	}
+
+}
