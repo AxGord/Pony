446c794dc243a170473d32d8b84647d4857c90b5
Alexander Gordeyko <axgord@gmail.com>
2020-01-26 03:45:28 +0300

    Pony Tools: Upgrade Build

--- tools/src/module/Build.hx
+++ tools/src/module/Build.hx
@@ -1,29 +1,38 @@
 package module;
 
 import pony.Fast;
-import types.BASection;
+import pony.SPair;
 import sys.FileSystem;
 import sys.io.File;
 import sys.net.Socket;
 import sys.net.Host;
 
-typedef LastCompilationOptions = {
-	command:Array<String>,
-	debug:Bool,
-	compiler:String
+import types.BASection;
+import module.Build.HAXE;
+import module.Build.HXML;
+import module.Build.D;
+
+private typedef LastCompilationOptions = {
+	command: Array<SPair<String>>,
+	debug: Bool,
+	compiler: String
 }
 
 /**
  * Build module
  * @author AxGord <axgord@gmail.com>
  */
-class Build extends CfgModule<BuildConfig> {
+@:final class Build extends CfgModule<BuildConfig> {
 
+	public static inline var HAXE: String = 'haxe';
+	public static inline var HXML: String = 'hxml';
+	public static inline var D: String = '-D';
 	private static inline var PRIORITY: Int = 1;
 	private static inline var TIMEOUT: Int = 5;
+	private static inline var LIB: String = '-lib';
 
 	private var flags(default, null): Array<String> = [];
-	private var haxelib: Array<String> = [];
+	private var haxelib: Array<String>;
 	private var postHaxelibs: Array<String> = [];
 	private var server: Bool = false;
 	private var lastCompilationOptions: LastCompilationOptions;
@@ -33,30 +42,14 @@ class Build extends CfgModule<BuildConfig> {
 
 	override public function init(): Void {
 		if (xml == null) return;
-		if (modules.xml.hasNode.haxelib) {
-			for (e in modules.xml.node.haxelib.nodes.lib) {
-				var a = e.innerData.split(' ');
-				haxelib.push('-lib');
-				haxelib.push(a.join(':'));
-			}
-		}
+		haxelib = modules.xml.hasNode.haxelib ?
+			[ for (e in modules.xml.node.haxelib.nodes.lib) e.innerData.split(' ').join(':') ] : [];
 		server = modules.xml.hasNode.server && modules.xml.node.server.hasNode.haxe;
 		initSections(PRIORITY, BASection.Build);
 	}
 
-	public function addHaxelib(lib: String): Void {
-		if (postHaxelibs.indexOf(lib) == -1) {
-			postHaxelibs.push('-lib');
-			postHaxelibs.push(lib);
-		}
-	}
-
-	public function addFlag(flag: String): Void {
-		if (flags.indexOf(flag) == -1) {
-			flags.push('-D');
-			flags.push(flag);
-		}
-	}
+	public inline function addHaxelib(lib: String): Void if (postHaxelibs.indexOf(lib) == -1) postHaxelibs.push(lib);
+	public inline function addFlag(flag: String): Void if (flags.indexOf(flag) == -1) flags.push(flag);
 
 	override private function readNodeConfig(xml: Fast, ac: AppCfg): Void {
 		new BuildConfigReader(xml, {
@@ -65,7 +58,7 @@ class Build extends CfgModule<BuildConfig> {
 			before: false,
 			section: BASection.Build,
 			command: [],
-			haxeCompiler: 'haxe',
+			haxeCompiler: HAXE,
 			hxml: null,
 			runHxml: [],
 			allowCfg: true
@@ -74,46 +67,32 @@ class Build extends CfgModule<BuildConfig> {
 
 	override private function runNode(cfg: BuildConfig): Void {
 		if (cfg.runHxml.length == 0) {
-			var cmd = cfg.command.concat(haxelib);
-			cmd = cmd.concat(flags).concat(postHaxelibs);
-			if (cfg.app != null) {
-				cmd.push('-D');
-				cmd.push('app=${cfg.app}');
-			}
-			if (cfg.debug) {
-				cmd.push('-debug');
-			}
+			var cmd: Array<SPair<String>> = cfg.command.copy();
+			for (l in haxelib) cmd.push(new SPair(LIB, l));
+			for (d in flags) cmd.push(new SPair(D, d));
+			for (l in postHaxelibs) cmd.push(new SPair(LIB, l));
+			if (cfg.app != null) cmd.push(new SPair(D, 'app=${cfg.app}'));
+			if (cfg.debug) cmd.push(new SPair('-debug', ''));
 			if (cfg.hxml != null) {
 				saveHxml(cfg.hxml, cmd);
 			} else {
 				runCompilation(cmd, cfg.debug, cfg.haxeCompiler);
 			}
-		}
-		for (e in cfg.runHxml) {
-			var cmd = cfg.command.copy();
-			cmd = cmd.concat(flags).concat(postHaxelibs);
-			cmd.push(e + '.hxml');
-			if (cfg.app != null) {
-				cmd.push('-D');
-				cmd.push('app=${cfg.app}');
-			}
-			if (cfg.debug) {
-				cmd.push('-debug');
-			}
+		} else for (e in cfg.runHxml) {
+			var cmd: Array<SPair<String>> = cfg.command.copy();
+			for (d in flags) cmd.push(new SPair(D, d));
+			for (l in postHaxelibs) cmd.push(new SPair(LIB, l));
+			cmd.push(new SPair(e + '.$HXML', ''));
+			if (cfg.app != null) cmd.push(new SPair(D, 'app=${cfg.app}'));
+			if (cfg.debug) cmd.push(new SPair('-debug', ''));
 			runCompilation(cmd, cfg.debug, cfg.haxeCompiler);
 		}
 		checkCompilation();
 	}
 
-	private function saveHxml(name: String, commands: Array<String>): Void {
-		name += '.hxml';
-		var f: Bool = false;
-		var s: String = '';
-		for (e in commands) {
-			s += e;
-			s += f ? '\n' : ' ';
-			f = !f;
-		}
+	private function saveHxml(name: String, commands: Array<SPair<String>>): Void {
+		name += '.$HXML';
+		var s: String = cmdArrPairToArrStr(commands).join('\n');
 		var prev: String = FileSystem.exists(name) ? File.getContent(name) : null;
 		if (prev != s) {
 			if (FileSystem.exists(Uglify.CACHE_FILE)) FileSystem.deleteFile(Uglify.CACHE_FILE);
@@ -121,23 +100,24 @@ class Build extends CfgModule<BuildConfig> {
 		}
 	}
 
-	private function runCompilation(command: Array<String>, debug: Bool, compiler: String): Void {
-		if (debug && server && compiler == 'haxe') {
-			var newline = "\n";
+	private function runCompilation(command: Array<SPair<String>>, debug: Bool, compiler: String): Void {
+		if (debug && server && compiler == HAXE) {
+			var newline: String = "\n";
 			tryCounter = 3;
 			var s: Socket = connectToHaxeServer();
-			var d = Sys.getCwd();
+			var d: String = Sys.getCwd();
 			s.write('--cwd ' + d + newline);
-			var cmd = command.join(newline);
-			Sys.println(cmd);
-			s.write(cmd);
+			for (c in command) {
+				var l: String = cmdPairToStr(c) + newline;
+				Sys.println(l);
+				s.write(l);
+			}
 			s.write("\000");
-
-			var hasError = false;
-			var r:String = null;
+			var hasError: Bool = false;
+			var r: String = null;
 			try {
 				r = s.read();
-			} catch (e:Any) {
+			} catch (e: Any) {
 				compilationServerError(Std.string(e));
 				return;
 			}
@@ -153,20 +133,22 @@ class Build extends CfgModule<BuildConfig> {
 			}
 			s.close();
 			if (hasError) Sys.exit(1);
-			lastCompilationOptions = {command:command, debug:debug, compiler:compiler};
+			lastCompilationOptions = { command: command, debug: debug, compiler: compiler };
 		} else {
-			Utils.command(compiler, command);
+			Utils.command(compiler, cmdArrPairToArrStr(command));
 		}
 	}
 
+	private static inline function cmdPairToStr(p: SPair<String>): String return p.a + (p.b.length > 0 ? ' ' + p.b : '');
+	private static inline function cmdArrPairToArrStr(a: Array<SPair<String>>): Array<String> return [ for (c in a) cmdPairToStr(c) ];
+
 	private function connectToHaxeServer(): Socket {
 		var port: Int = Std.parseInt(modules.xml.node.server.node.haxe.innerData);
-		var s: Socket = null;
 		while (true) try {
-			s = new Socket();
+			var s: Socket = new Socket();
 			s.connect(new Host('127.0.0.1'), port);
 			return s;
-		} catch (e:Any) {
+		} catch (e: Any) {
 			compilationServerError(Std.string(e));
 		}
 		return null;
@@ -181,7 +163,7 @@ class Build extends CfgModule<BuildConfig> {
 			Sys.println('Connect error, try again after $TIMEOUT sec...');
 			Sys.sleep(TIMEOUT);
 			if (lastCompilationOptions != null) {
-				var lco = lastCompilationOptions;
+				var lco: LastCompilationOptions = lastCompilationOptions;
 				lastCompilationOptions = null;
 				runCompilation(lco.command, lco.debug, lco.compiler);
 			}
@@ -189,17 +171,15 @@ class Build extends CfgModule<BuildConfig> {
 	}
 
 	private function checkCompilation(): Void {
-		if (lastCompilationOptions != null && lastCompilationOptions.debug && server && lastCompilationOptions.compiler == 'haxe') {
-			var s = connectToHaxeServer();
-			s.close();
-		}
+		if (lastCompilationOptions != null && lastCompilationOptions.debug && server && lastCompilationOptions.compiler == HAXE)
+			connectToHaxeServer().close();
 	}
 
 }
 
 private typedef BuildConfig = {
 	> types.BAConfig,
-	command: Array<String>,
+	command: Array<SPair<String>>,
 	haxeCompiler: String,
 	hxml: String,
 	runHxml: Array<String>
@@ -214,23 +194,22 @@ private class BuildConfigReader extends BAReader<BuildConfig> {
 			super.readNode(xml);
 		} catch (s: String) {
 			if (s.substr(0, UT.length) == UT) {
+				var d: String = try {
+					normalize(xml.innerData);
+				} catch (_: Dynamic) {
+					'';
+				}
 				switch xml.name {
-					case 'hxml':
-						cfg.runHxml.push(StringTools.trim(xml.innerData));
+					case HXML:
+						cfg.runHxml.push(d);
 					case 'd':
-						cfg.command.push('-D');
-						if (xml.has.name)
-							cfg.command.push(xml.att.name + '=' + xml.innerData);
-						else
-							cfg.command.push(xml.innerData);
+						cfg.command.push(new SPair(D, xml.has.name ? normalize(xml.att.name) + '=' + d : d));
 					case 'i':
-							cfg.command.push('--macro');
-							cfg.command.push('"include(\'${xml.innerData}\')"');
+						cfg.command.push(new SPair('--macro', '"include(\'$d\')"'));
+					case 'interp':
+						cfg.command.push(new SPair('--interp', ''));
 					case a:
-						cfg.command.push('-' + a);
-						try {
-							cfg.command.push(xml.innerData);
-						} catch (_: Dynamic) {}
+						cfg.command.push(new SPair('-$a', d));
 				}
 			} else {
 				throw s;
@@ -240,15 +219,15 @@ private class BuildConfigReader extends BAReader<BuildConfig> {
 
 	override private function clean(): Void {
 		cfg.command = [];
-		cfg.haxeCompiler = 'haxe';
+		cfg.haxeCompiler = HAXE;
 		cfg.hxml = null;
 		cfg.runHxml = [];
 	}
 
 	override private function readAttr(name: String, val: String): Void {
 		switch name {
-			case 'haxe': cfg.haxeCompiler = val;
-			case 'hxml': cfg.hxml = val;
+			case HAXE: cfg.haxeCompiler = val;
+			case HXML: cfg.hxml = val;
 			case _:
 		}
 	}
