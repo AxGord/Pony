d1f6d68be9a90d53b1a99198db36004370818660
Alexander Gordeyko <axgord@gmail.com>
2020-05-13 13:12:41 +0300

    Update haxelib v 1.2.8. Pony Tools: Auto commit haxelib update

--- tools/src/Haxelib.hx
+++ tools/src/Haxelib.hx
@@ -1,17 +1,19 @@
+import sys.FileSystem;
+
 /**
  * Haxelib
  * @author AxGord <axgord@gmail.com>
  */
 class Haxelib {
-	
-	private static inline var listFile:String = 'haxelibfiles.txt';
-	private static inline var outputFile:String = 'haxelib.zip';
-	private static inline var haxelibFile:String = 'haxelib.json';
-	private static inline var readmeFile:String = 'README.md';
-	private static inline var badgeVersionBegin:String = '[![Haxelib](https://img.shields.io/badge/haxelib-';
-	private static inline var badgeVersionEnd:String = '-';
 
-	public static function run(command:String, args:Array<String>):Void {
+	private static inline var listFile: String = 'haxelibfiles.txt';
+	private static inline var outputFile: String = 'haxelib.zip';
+	private static inline var haxelibFile: String = 'haxelib.json';
+	private static inline var readmeFile: String = 'README.md';
+	private static inline var badgeVersionBegin: String = '[![Haxelib](https://img.shields.io/badge/haxelib-';
+	private static inline var badgeVersionEnd: String = '-';
+
+	public static function run(command: String, args: Array<String>): Void {
 		switch command {
 			case 'submit':
 				var a = args.shift();
@@ -34,7 +36,7 @@ class Haxelib {
 		}
 	}
 
-	private static function upver(index:Int, desc:String):Void {
+	private static function upver(index: Int, desc: String): Void {
 		var jdata = getData();
 		var ver = parseVersion(jdata.version);
 		ver[index]++;
@@ -42,7 +44,7 @@ class Haxelib {
 		_submit(jdata, ver.join('.'), desc);
 	}
 
-	private static function getData():Dynamic {
+	private static function getData(): Dynamic {
 		if (!libexists()) {
 			Utils.error('$haxelibFile not exists');
 			return null;
@@ -52,14 +54,14 @@ class Haxelib {
 		return jdata;
 	}
 
-	public static function submit(version:String, desc:String):Void {
+	public static function submit(version: String, desc: String): Void {
 		if (version == null) {
 			Utils.error('Please, set version');
 			return;
 		}
 
 		var jdata = getData();
-		
+
 		if (jdata.version == version) {
 			Utils.error('Wrong new version');
 			return;
@@ -76,39 +78,46 @@ class Haxelib {
 		_submit(jdata, version, desc);
 	}
 
-	private static function getVersion():String return getData().version;
+	private static function getVersion(): String return getData().version;
 
-	private static function updateReadme(version:String):Void {
+	private static function updateReadme(version: String): Void {
 		pony.text.TextTools.betweenReplaceFile(readmeFile, badgeVersionBegin, badgeVersionEnd, version);
 	}
 
-	private static function _submit(jdata:Dynamic, version:String, desc:String):Void {
+	private static function _submit(jdata: Dynamic, version: String, desc: String): Void {
 		updateReadme(version);
 		if (desc == null) desc = '';
 		jdata.version = version;
 		jdata.releasenote = desc;
-
 		saveJson(jdata);
-
 		upload();
+		git(version, desc);
 	}
 
-	private static function upload():Void {
+	private static function upload(): Void {
 		var data = sys.io.File.getContent(listFile).split('\n');
 		if (data.indexOf(haxelibFile) == -1) data.push(haxelibFile);
 		var zip = new pony.ZipTool(outputFile, 0);
 		zip.onLog << Sys.println;
-		zip.onError << function(err:String) throw err;
+		zip.onError << function(err: String) throw err;
 		zip.writeList(data).end();
 
-		Sys.command('haxelib', ['submit', outputFile]);
-
-		sys.FileSystem.deleteFile(outputFile);
+		// Sys.command('haxelib', ['submit', outputFile]);
+		FileSystem.deleteFile(outputFile);
 	}
 
-	private static function parseVersion(s:String):Array<Int> return s.split('.').map(Std.parseInt);
+	private static function git(version: String, desc: String): Void {
+		if (!FileSystem.exists('.git')) return;
+		try {
+			var message: String = StringTools.rtrim('Update haxelib v $version. $desc');
+			Utils.command('git', ['commit', '-a', '-m', message]);
+			Utils.command('git', ['push']);
+		} catch (e: Dynamic) {}
+	}
 
-	public static function create(name:String, author:String):Void {
+	private static function parseVersion(s: String): Array<Int> return s.split('.').map(Std.parseInt);
+
+	public static function create(name: String, author: String): Void {
 		if (name == null) {
 			Utils.error('Please, set lib name (first arg)');
 			return;
@@ -130,15 +139,14 @@ class Haxelib {
 			version: "0.0.1",
 			releasenote: "Init",
 			contributors: [author],
-			dependencies: {
-			}
+			dependencies: {}
 		};
 		saveJson(jdata);
 		Sys.println('Library $name created');
 	}
 
-	private static inline function libexists():Bool return sys.FileSystem.exists(haxelibFile);
+	private static inline function libexists(): Bool return FileSystem.exists(haxelibFile);
 
-	private static inline function saveJson(jdata:Dynamic):Void Utils.saveJson(haxelibFile, jdata);
+	private static inline function saveJson(jdata: Dynamic): Void Utils.saveJson(haxelibFile, jdata);
 
 }
