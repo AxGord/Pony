5906640e9f1911111a84f4937744997c816e5e57
Alexander Gordeyko <axgord@gmail.com>
2020-11-18 23:12:44 +0300

    ZmqClient refactoring

--- src/pony/net/SocketClient.hx
+++ src/pony/net/SocketClient.hx
@@ -1,5 +1,7 @@
 package pony.net;
 
+import haxe.Int64;
+import haxe.io.Bytes;
 import haxe.io.BytesInput;
 import haxe.io.BytesOutput;
 import pony.events.Signal1;
@@ -21,12 +23,13 @@ class SocketClient
 	private static inline var DEFAULT_LEN_BLOCK_SIZE: Int = 4;
 
 	@:auto public var onTask: Signal2<BytesInput, SocketClient>;
+	@:auto public var onTaskError: Signal1<SocketClient>;
 
 	public var writeLengthSize: UInt;
 
 	private var stack: Array<BytesOutput>;
 	private var taskPrefix: BytesInput;
-	private var taskDataLength: Int = -1;
+	private var taskDataLength: Int64 = -1;
 	private var taskBuffer: BytesOutput;
 
 	override function sharedInit(): Void {
@@ -79,21 +82,29 @@ class SocketClient
 	public function sendStack(): Void if (stack.length > 0) send(stack.shift());
 	public function sendAllStack(): Void while (stack.length > 0) send(stack.shift());
 
-	public inline function setTask(?prefix: BytesInput, len: UInt): Signal2<BytesInput, SocketClient> {
+	public inline function setTaskb(prefix: Bytes, ?len: Int64): Signal2<BytesInput, SocketClient> {
+		var bo: BytesOutput = new BytesOutput();
+		bo.write(prefix);
+		return setTask(bo, len);
+	}
+
+	public inline function setTask(?prefix: BytesOutput, ?len: Int64): Signal2<BytesInput, SocketClient> {
 		taskBuffer = new BytesOutput();
-		taskPrefix = prefix;
-		taskDataLength = len;
+		taskPrefix = prefix == null ? null : new BytesInput(prefix.getBytes());
+		taskDataLength = len == null ? 0 : len;
 		onData << taskDataHandler;
 		return onTask;
 	}
 
 	public inline function removeTask(): Void {
+		taskBuffer = null;
 		taskPrefix = null;
 		taskDataLength = -1;
 		onData >> taskDataHandler;
 	}
 
 	private function taskDataHandler(bi: BytesInput): Void {
+		if (taskDataLength == -1) return;
 		if (taskPrefix != null) {
 			if (bi.length < taskPrefix.length - taskPrefix.position) {
 				if (taskPrefix.read(bi.length).compare(bi.readAll()) != 0) taskError();
@@ -105,19 +116,43 @@ class SocketClient
 				taskPrefix = null;
 			}
 		}
-		taskBuffer.write(bi.readAll());
-		if (taskBuffer.length >= taskDataLength) {
-			var b: BytesInput = new BytesInput(taskBuffer.getBytes());
-			var r: BytesInput = new BytesInput(b.read(taskDataLength));
+		if (taskDataLength == 0) {
+			var b: BytesInput = new BytesInput(bi.readAll());
 			removeTask();
-			eTask.dispatch(r, this);
-			taskDataHandler(new BytesInput(b.readAll()));
+			eTask.dispatch(null, this);
+			taskDataHandler(b);
+		} else {
+			taskBuffer.write(bi.readAll());
+			if (taskBuffer.length >= taskDataLength) {
+				var b: BytesInput = new BytesInput(taskBuffer.getBytes());
+				var r: BytesInput = new BytesInput(b.read(taskDataLength.low)); // todo high
+				removeTask();
+				eTask.dispatch(r, this);
+				taskDataHandler(new BytesInput(b.readAll()));
+			}
 		}
 	}
 
 	private inline function taskError(): Void {
 		removeTask();
-		eTask.dispatch(null, this);
+		eTaskError.dispatch(this);
+	}
+
+	public function sendSetTask(out: Bytes, ?prefix: Bytes, len: UInt = 0): Signal2<BytesInput, SocketClient> {
+		var ob: BytesOutput = new BytesOutput();
+		ob.write(out);
+		send(ob);
+		if (prefix != null) {
+			var pb: BytesOutput = new BytesOutput();
+			pb.write(prefix);
+			return setTask(pb, len);
+		} else {
+			return setTask(len);
+		}
+	}
+
+	public inline function sendSetTaskSym(b: Bytes, len: UInt = 0): Signal2<BytesInput, SocketClient> {
+		return sendSetTask(b, b, len);
 	}
 
 }
--- src/pony/net/zmq/ZmqClient.hx
+++ src/pony/net/zmq/ZmqClient.hx
@@ -1,5 +1,6 @@
 package pony.net.zmq;
 
+import haxe.Int64;
 import pony.events.Signal0;
 import pony.events.Signal1;
 import pony.events.Signal2;
@@ -10,24 +11,17 @@ import pony.net.SocketClient;
 
 class ZmqClient extends Logable {
 
-	private static inline var RESPONCE_CODE_LENGTH: Int = 1;
-	private static inline var FIRST_MESSAGE_RESPONCE_LENGTH_A: Int = FIRST_MESSAGE_RESPONCE_LENGTH_B + RESPONCE_CODE_LENGTH;
-	private static inline var FIRST_MESSAGE_RESPONCE_LENGTH_B: Int = 10;
-	private static inline var SECOND_MESSAGE_RESPONCE_LENGTH_A: Int = 52;
-	private static inline var SECOND_MESSAGE_RESPONCE_LENGTH_B: Int = 80;
-	private static var REQUEST_RESPONSE_CODE: Array<Int> = [0x03];
-	private static var FIRST_MESSAGE_REQUEST: Array<Int> = [0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x7F];
-	private static var SECOND_MESSAGE_REQUEST: Array<Int> = [0x00, 0x4E, 0x55, 0x4C, 0x4C].concat([for (_ in 0...48) 0x00]);
-	private static var THIRD_MESSAGE: Array<Int> = [0x05, 0x52, 0x45, 0x41, 0x44, 0x59, 0x0B, 0x53, 0x6F, 0x63, 0x6B, 0x65, 0x74, 0x2D,
-		0x54, 0x79, 0x70, 0x65, 0x00, 0x00, 0x00, 0x03, 0x52, 0x45]; // 0003  5355 4200 0101 for publisher
-	private static var CLIENT_CONCAT_THIRD_PREFIX: Array<Int> = [0x04, 0x19];
-	private static var CLIENT_CONCAT_THIRD_POSTFIX: Array<Int> = [0x50];
-	private static var CLIENT_THIRD_PREFIX: Array<Int> = [0x04, 0x26];
-	private static var CLIENT_THIRD_POSTFIX: Array<Int> = [0x51, 0x08, 0x49, 0x64, 0x65, 0x6E, 0x74, 0x69, 0x74, 0x79, 0x00, 0x00, 0x00, 0x00];
-	private static var SERVER_THIRD_PREFIX: Array<Int> = [0x04, 0x19];
-	private static var SERVER_THIRD_POSTFIX: Array<Int> = [0x50];
-	private static inline var DATA_CODE: Int = 0x01;
 	private static inline var MAX_BYTE: Int = 0xFF;
+
+	private static var REQUEST_RESPONSE_CODE: Bytes = Bytes.ofHex('03');
+	private static var FIRST_MESSAGE_REQUEST: Bytes = Bytes.ofHex('FF00000000000000017F');
+	private static var SECOND_MESSAGE_REQUEST: Bytes = Bytes.ofHex('004E554C4C' + [for (_ in 0...48) '00'].join(''));
+	private static var THIRD_MESSAGE: Bytes = Bytes.ofHex('0552454144590B536F636B65742D54797065000000035245'); // 0003  5355 4200 0101 for publisher
+	private static var CLIENT_THIRD_PREFIX: Bytes = Bytes.ofHex('0426');
+	private static var CLIENT_THIRD_POSTFIX: Bytes = Bytes.ofHex('51084964656E7469747900000000');
+	private static var SERVER_THIRD_PREFIX: Bytes = Bytes.ofHex('0419');
+	private static var SERVER_THIRD_POSTFIX: Bytes = Bytes.ofHex('50');
+	private static var DATA_CODE: Bytes = Bytes.ofHex('0100');
 
 	@:auto public var onOpen: Signal0;
 	@:auto public var onData: Signal1<BytesInput>;
@@ -39,79 +33,68 @@ class ZmqClient extends Logable {
 	public function new(host: String = '127.0.0.1', port: Int) {
 		super();
 		socket = new SocketClient(host, port, -1, 0, false);
+		socket.onTaskError << fatal.bind('task error');
 		listenErrorAndLog(socket, 'ZMQ');
 		socket.onConnect < connectHandler;
 	}
 
-	private function connectHandler(): Void {
-		sendBytes([FIRST_MESSAGE_REQUEST]) < firstDataHandler;
+	private static function concatBytes(bs: Array<Bytes>): Bytes {
+		var bo: BytesOutput = new BytesOutput();
+		for (b in bs) bo.write(b);
+		return bo.getBytes();
 	}
 
-	private function firstDataHandler(bi: BytesInput): Void {
-		switch bi.length {
-			case FIRST_MESSAGE_RESPONCE_LENGTH_A if (checkBytes(bi, [FIRST_MESSAGE_REQUEST, REQUEST_RESPONSE_CODE])):
-				sendBytes([REQUEST_RESPONSE_CODE, SECOND_MESSAGE_REQUEST]) < secondDataHander;
-			case FIRST_MESSAGE_RESPONCE_LENGTH_B if (checkBytes(bi, [FIRST_MESSAGE_REQUEST])):
-				sendBytes([REQUEST_RESPONSE_CODE]) < first03DataHander;
-			case _:
-				wrongBytes(bi, 'first');
-		}
-	}
-
-	private function first03DataHander(bi: BytesInput): Void {
-		if (bi.length == RESPONCE_CODE_LENGTH && checkBytes(bi, [REQUEST_RESPONSE_CODE])) {
-			first03complete = 1;
-			sendBytes([SECOND_MESSAGE_REQUEST]) < secondDataHander;
-		} else {
-			wrongBytes(bi, 'first 03');
-		}
-	}
-
-	private function secondDataHander(bi: BytesInput): Void {
-		switch bi.length - first03complete {
-			case SECOND_MESSAGE_RESPONCE_LENGTH_A if (checkBytes(bi, [SECOND_MESSAGE_REQUEST])):
-				sendBytes([CLIENT_THIRD_PREFIX, THIRD_MESSAGE, CLIENT_THIRD_POSTFIX]) < thirdDataHander;
-
-			case SECOND_MESSAGE_RESPONCE_LENGTH_B
-			if (checkBytes(bi, [SECOND_MESSAGE_REQUEST, CLIENT_CONCAT_THIRD_PREFIX, THIRD_MESSAGE, CLIENT_CONCAT_THIRD_POSTFIX])):
-				sendBytes([CLIENT_THIRD_PREFIX, THIRD_MESSAGE, CLIENT_THIRD_POSTFIX]);
-				handshake();
-
-			case _:
-				wrongBytes(bi, 'second');
-		}
-	}
-
-	private function thirdDataHander(bi: BytesInput): Void {
-		if (checkBytes(bi, [SERVER_THIRD_PREFIX, THIRD_MESSAGE, SERVER_THIRD_POSTFIX]))
-			handshake();
-		else
-			wrongBytes(bi, 'third');
-	}
+	private function connectHandler(): Void socket.sendSetTaskSym(FIRST_MESSAGE_REQUEST) < firstTaskHandler;
+	private function firstTaskHandler(): Void socket.sendSetTaskSym(REQUEST_RESPONSE_CODE) < secondTaskHandler;
+	private function secondTaskHandler(): Void socket.sendSetTaskSym(SECOND_MESSAGE_REQUEST) < thirdTaskHandler;
+	private function thirdTaskHandler(): Void socket.sendSetTask(
+		concatBytes([CLIENT_THIRD_PREFIX, THIRD_MESSAGE, CLIENT_THIRD_POSTFIX]),
+		concatBytes([SERVER_THIRD_PREFIX, THIRD_MESSAGE, SERVER_THIRD_POSTFIX])
+	) < handshake;
 
 	private function handshake(): Void {
-		log('Handshake');
+		log('ZMQ Handshake');
 		opened = true;
-		socket.onData << dataHandler;
+		listenData();
 		for (bo in stack) socket.send(bo);
 		stack = [];
 		eOpen.dispatch();
 	}
 
+	private function listenData(): Void socket.setTaskb(DATA_CODE, 1) < dataFirstHandler;
+
+	private function dataFirstHandler(bi: BytesInput): Void {
+		switch bi.readByte() {
+			case 0x00: socket.setTask(1) < lenFirstHandler;
+			case 0x02: socket.setTask(8) < lenSecondHandler;
+			case _: fatal('data');
+		}
+	}
+
+	private inline function listenLen(len: Int64): Void {
+		socket.onTask < eData;
+		socket.setTask(len) < listenData;
+	}
+
+	private function lenFirstHandler(bi: BytesInput): Void listenLen(bi.readByte());
+	private function lenSecondHandler(bi: BytesInput): Void {
+		bi.bigEndian = true;
+		listenLen(Int64.make(bi.readInt32(), bi.readInt32()));
+	}
+
 	public function send(bo: BytesOutput): Void {
 		var r: BytesOutput = new BytesOutput();
+		r.bigEndian = true;
 		if (bo.length <= MAX_BYTE) {
-			r.writeByte(DATA_CODE);
-			r.writeByte(0x00);
+			r.write(DATA_CODE);
 			r.writeByte(0x00);
 			r.writeByte(bo.length);
 		} else {
-			r.writeByte(DATA_CODE);
-			r.writeByte(0x00);
+			r.write(DATA_CODE);
 			r.writeByte(0x02);
-			for (_ in 0...5) r.writeByte(0x00);
-			r.writeByte(DATA_CODE);
-			r.writeByte(bo.length - MAX_BYTE);
+			var len: Int64 = bo.length;
+			r.writeInt32(len.high);
+			r.writeInt32(len.low);
 		}
 		r.write(bo.getBytes());
 		if (opened)
@@ -120,61 +103,13 @@ class ZmqClient extends Logable {
 			stack.push(r);
 	}
 
-	private function dataHandler(bi: BytesInput): Void {
-		if (bi.readByte() == DATA_CODE && bi.readByte() == 0x00) {
-			switch bi.readByte() {
-				case 0x00:
-					eData.dispatch(new BytesInput(bi.read(bi.readByte())));
-				case 0x02:
-					for (_ in 0...5) if (bi.readByte() != 0x00) {
-						wrongBytes(bi, 'data');
-						return;
-					}
-					if (bi.readByte() != 0x01) {
-						wrongBytes(bi, 'data');
-						return;
-					}
-					eData.dispatch(new BytesInput(bi.read(bi.readByte() + MAX_BYTE)));
-				case _:
-					wrongBytes(bi, 'data');
-			}
-		} else {
-			wrongBytes(bi, 'data');
-		}
-	}
-
-	private function sendBytes(arrays: Array<Array<Int>>): Signal2<BytesInput, SocketClient> {
-		socket.send(makeBytes(arrays));
-		return socket.onData;
-	}
-
-	private static function makeBytes(arrays: Array<Array<Int>>): BytesOutput {
-		var bo: BytesOutput = new BytesOutput();
-		addBytes(bo, arrays);
-		return bo;
-	}
-
-	private static function addBytes(bo: BytesOutput, arrays: Array<Array<Int>>): BytesOutput {
-		for (a in arrays) for (b in a) bo.writeByte(b);
-		return bo;
-	}
-
-	private static function checkBytes(bi: BytesInput, arrays: Array<Array<Int>>): Bool {
-		for (a in arrays) for (b in a) if (b != bi.readByte()) return false;
-		return true;
-	}
-
-	public function wrongBytes(bi: BytesInput, packetName: String): Void {
-		fatal('Wrong $packetName message, length: ${bi.length}');
-	}
-
 	public function fatal(message: String): Void {
 		error(message);
 		destroy();
 	}
 
 	public function destroy(): Void {
-		socket.destroySignals();
+		destroySignals();
 		socket.destroy();
 	}
 
