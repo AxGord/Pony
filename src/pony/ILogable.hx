e40f47e05a07c82dc991c286089f071408b7d0f0
Alexander Gordeyko <axgord@gmail.com>
2021-01-10 02:57:47 +0300

    ILogable rm active

--- src/pony/ILogable.hx
+++ src/pony/ILogable.hx
@@ -10,8 +10,6 @@ import pony.events.Event2;
  */
 interface ILogable {
 
-	var logActive(get, never): Bool;
-	var errorActive(get, never): Bool;
 	var onLog(get, never): Signal2<String, PosInfos>;
 	var onError(get, never): Signal2<String, PosInfos>;
 	function error(s: String, ?p: PosInfos): Void;
