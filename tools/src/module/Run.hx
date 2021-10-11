8b79634b57daec56ca5aa26e43e75b77752c42bf
Alexander Gordeyko <axgord@gmail.com>
2021-10-11 19:25:15 +0300

    Pony Tools: upgrade run

--- tools/src/module/Run.hx
+++ tools/src/module/Run.hx
@@ -1,68 +1,94 @@
 package module;
 
 import pony.Fast;
+
 import types.BASection;
 import types.BAConfig;
 
-typedef RunConfig = { > BAConfig,
-	path: String,
-	cmd: String
+typedef RunConfig = {
+	> BAConfig,
+	?path: String,
+	command: Array<{?path: String, cmd: String}>,
 }
 
 /**
  * Run module
  * @author AxGord <axgord@gmail.com>
  */
-class Run extends CfgModule<RunConfig> {
+@:nullSafety(Strict) class Run extends CfgModule<RunConfig> {
 
-	private static inline var PRIORITY:Int = 0;
+	private static inline var PRIORITY: Int = 0;
 
 	public function new() super('run');
 
-	override public function init():Void initSections(PRIORITY, BASection.Run);
+	override public function init(): Void initSections(PRIORITY, BASection.Run);
 
-	override private function readNodeConfig(xml:Fast, ac:AppCfg):Void {
+	override private function readNodeConfig(xml: Fast, ac: AppCfg): Void {
 		new RunReader(xml, {
 			debug: ac.debug,
 			app: ac.app,
 			before: false,
 			section: BASection.Run,
 			path: null,
-			cmd: null,
+			command: [],
 			allowCfg: true
 		}, configHandler);
 	}
 
-	override private function runNode(cfg:RunConfig):Void {
-		var cwd = new Cwd(cfg.path);
-		cwd.sw();
+	override private function runNode(cfg: RunConfig): Void {
+		var path: String = cfg.path != null ? cfg.path : '';
+		for (cmd in cfg.command) {
+			var p: String = path + (cmd.path != null ? cmd.path : '');
+			var cwd = new Cwd(p);
+			if (p != '') cwd.sw();
 
-		var args = cfg.cmd.split(' ');
-		var cmd = args.shift();
-		Utils.command(cmd, args);
+			var args = cmd.cmd.split(' ');
+			@:nullSafety(Off) var cmd: String = args.shift();
+			Utils.command(cmd, args);
 
-		cwd.sw();
+			if (p != '') cwd.sw();
+		}
 	}
 
 }
 
 private class RunReader extends BAReader<RunConfig> {
 
-	override private function clean():Void {
+	override private function clean(): Void {
 		cfg.path = null;
-		cfg.cmd = null;
+		cfg.command = [];
 	}
 
-	override private function readXml(xml:Fast):Void {
-		for (a in xml.x.attributes()) readAttr(a, normalize(xml.x.get(a)));
-		cfg.cmd = normalize(xml.innerData);
-		if (allowEnd) end();
+	override private function readXml(xml: Fast): Void {
+		try {
+			cfg.command.push({cmd: normalize(xml.innerData)});
+			for (a in xml.x.attributes())
+				readAttr(a, normalize(xml.x.get(a)));
+			if (allowEnd)
+				end();
+		} catch (e: Dynamic) {
+			super.readXml(xml);
+		}
 	}
 
-	override private function readAttr(name:String, val:String):Void {
+	override private function readAttr(name: String, val: String): Void {
 		switch name {
-			case 'path': cfg.path = val;
+			case 'path':
+				cfg.path = val;
 			case _:
+		}
+	}
+
+	override private function readNode(xml: Fast): Void {
+		switch xml.name {
+			case 'command':
+				cfg.command.push({cmd: normalize(xml.innerData), path: xml.has.path ? normalize(xml.att.path) : null});
+			case 'haxe':
+				cfg.command.push({cmd: 'haxe --run ' + normalize(xml.innerData), path: xml.has.path ? normalize(xml.att.path) : null});
+			case 'lime':
+				cfg.command.push({cmd: 'haxelib run lime ' + normalize(xml.innerData), path: xml.has.path ? normalize(xml.att.path) : null});
+			case _:
+				super.readNode(xml);
 		}
 	}
 
