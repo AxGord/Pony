608aa9497717aa4e697a9f21e5d48fd65a2ac611
Alexander Gordeyko <axgord@gmail.com>
2022-01-21 21:41:00 +0300

    Macro patch tool

--- src/pony/macro/Tools.hx
+++ src/pony/macro/Tools.hx
@@ -5,40 +5,77 @@ import haxe.macro.Context;
 import haxe.macro.Expr;
 
 /**
- * ...
+ * Macro Tools
  * @author AxGord
  */
-class Tools {
-	
+@:nullSafety(Strict) class Tools {
+
 	public static var staticPlatform:Bool = Context.defined('cs') || Context.defined('flash') || Context.defined('java');
 
 	public inline static function argsArray(func:Expr, args:Array<Expr>):Expr {
 		args.shift();
 		return macro $e{func} ($a{[[$a{args}]]});
 	}
-	
+
 	public static function argsArrayAbstr(obj:ExprOf<Function>, name:String, args:Array < Expr > ):Expr {
-		return macro $e{macro $ { obj } .$name} ($a{[[$a{args}]]});
+		return macro $e{macro ${ obj }.$name} ($a{[[$a{args}]]});
 	}
-	
+
 	public static function getMeta(a:Metadata, n:String, addHidding:Bool=false):MetadataEntry {
 		if (a == null) return null;
-		for (e in a) if (e.name == n || (addHidding && e.name == ':'+n)) return e;
+		for (e in a) if (e.name == n || (addHidding && e.name == ':' + n)) return e;
 		return null;
 	}
-	
+
 	public static function checkMeta(a:Metadata, an:Array<String>):Bool {
 		for (n in an) if (getMeta(a, n) != null) return true;
 		return false;
 	}
-	
+
 	public static function createInit():Field {
-		return {name: '__init__', access: [AStatic, APrivate], kind: FFun({args:[], ret: ComplexType.TPath({pack:[],name:'Void'}), expr: null}), pos: Context.currentPos()};
+		return {
+			name: '__init__',
+			access: [AStatic, APrivate],
+			kind: FFun({args:[], ret: ComplexType.TPath({pack:[],name:'Void'}), expr: null}),
+			pos: Context.currentPos()
+		};
 	}
-	
+
 	public static function createNew():Field {
 		return {name: 'new', access: [APublic], kind: FFun({args:[], ret: ComplexType.TPath({pack:[],name:'Void'}), expr: null}), pos: Context.currentPos()};
 	}
-	
+
+	public static function patch(methods: Map<String, Expr>, ?addToEnd: Map<String, Expr>): Array<Field> {
+		var fields: Array<Field> = Context.getBuildFields();
+		for (field in fields) {
+			var ex: Null<Expr> = methods[field.name];
+			if (ex != null) {
+				switch field.kind {
+					case FFun(f):
+						f.expr = ex;
+					case _:
+						Context.error('This is not method', field.pos);
+				}
+			}
+			if (addToEnd != null) {
+				var ex: Null<Expr> = addToEnd[field.name];
+				if (ex != null) {
+					switch field.kind {
+						case FFun(f):
+							switch f.expr.expr {
+								case EBlock(exprs):
+									exprs.push(ex);
+								case _:
+									Context.error('This is not block method', f.expr.pos);
+							}
+						case _:
+							Context.error('This is not method', field.pos);
+					}
+				}
+			}
+		}
+		return fields;
+	}
+
 }
 #end
